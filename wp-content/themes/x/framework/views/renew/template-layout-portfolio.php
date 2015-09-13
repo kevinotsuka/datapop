904351488c80411fa9cc460cb0a3aa63fea8dabe
kevinotsuka <otsukasan@gmail.com>
2015-09-14 02:12:30 +0900

    adding content again

--- wp-content/themes/x/framework/views/renew/template-layout-portfolio.php
+++ wp-content/themes/x/framework/views/renew/template-layout-portfolio.php
@@ -12,6 +12,7 @@
 
   <div class="x-container max width offset">
     <div class="<?php x_main_content_class(); ?>" role="main">
+	<?php $id = 205; $p = get_page($id); echo apply_filters(‘the_content’, $p->post_content); ?>
 
       <?php x_get_view( 'global', '_portfolio' ); ?>
 
