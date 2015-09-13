10431b93c228dfd6478986a8bbbbab3fb1c191d1
kevinotsuka <otsukasan@gmail.com>
2015-09-14 01:34:17 +0900

    adding content to portfolio template

--- wp-content/themes/x/framework/views/renew/template-layout-portfolio.php
+++ wp-content/themes/x/framework/views/renew/template-layout-portfolio.php
@@ -12,6 +12,7 @@
 
   <div class="x-container max width offset">
     <div class="<?php x_main_content_class(); ?>" role="main">
+    	<?php x_get_view( 'global', '_content', 'the-content' ); ?>
 
       <?php x_get_view( 'global', '_portfolio' ); ?>
 
