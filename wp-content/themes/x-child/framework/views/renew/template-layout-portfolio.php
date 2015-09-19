ffdcdff628b9cba79fdfe2ca44a4d9b898de5df4
kevinotsuka <otsukasan@gmail.com>
2015-09-19 16:49:50 +0900

    filter take two

--- wp-content/themes/x-child/framework/views/renew/template-layout-portfolio.php
+++ wp-content/themes/x-child/framework/views/renew/template-layout-portfolio.php
@@ -20,34 +20,6 @@
 
     <?php get_sidebar(); ?>
 
-<!---adding multi filter ------>
-
-<?php if ( x_get_content_layout() != 'full-width' ) : ?>
-
-<!----- START : THIS IS FILTER FORM UNDER wp-sidebar.php -------->
-<form action="<?php echo home_url()?>">
-<?php 
-  $categories = get_categories(); 
-  foreach ($categories as $category) {
-  echo '<input type="checkbox" name="cat_select[]" value="'.$category->cat_ID.'"> '.$category->category_nicename.'<br />';
-  }
-?>
-<button type="submit">Filter</button>
-</form>
-
-<!----- END : THIS IS FILTER FORM UNDER wp-sidebar.php -------->
-
-  <aside class="<?php x_sidebar_class(); ?>" role="complementary">
-    <?php if ( get_option( 'ups_sidebars' ) != array() ) : ?>
-      <?php dynamic_sidebar( apply_filters( 'ups_sidebar', 'sidebar-main' ) ); ?>
-    <?php else : ?>
-      <?php dynamic_sidebar( 'sidebar-main' ); ?>
-    <?php endif; ?>
-  </aside>
-
-<?php endif; ?>
-<!---adding multi filter end ------>
-
   </div>
 
 <?php get_footer(); ?>
