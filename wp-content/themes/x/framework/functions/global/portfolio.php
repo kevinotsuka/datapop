ce73757db6b8bee681ab9e976d7036774d90f9da
kevinotsuka <otsukasan@gmail.com>
2015-09-20 20:05:44 +0900

    php output clean up

--- wp-content/themes/x/framework/functions/global/portfolio.php
+++ wp-content/themes/x/framework/functions/global/portfolio.php
@@ -173,13 +173,15 @@ function x_portfolio_filters() {
           <ul class="x-portfolio-filters-menu unstyled">
             <div data-option-key="filter">
             <li><a href="#" data-option-value="*" class="x-portfolio-filter selected"><?php _e( 'All', '__x__' ); ?></a></li>
-            <?php foreach ( $terms as $term ) { if (md5( $term->slug ) != 'b94ad99e5dc27b3365d5d2aa77d73ff0') { ?> 
+            <?php foreach ( $terms as $term ) { if (md5( $term->slug ) != 'b94ad99e5dc27b3365d5d2aa77d73ff0' && md5( $term->slug ) != '7412df2b1db8cd2a5d4aafdb6c2090d3' && md5( $term->slug ) != 'c185ddac8b5a8f5aa23c5b80bc12d214') { ?> 
               <li><a href="#" data-option-value=".x-portfolio-<?php echo md5( $term->slug ); ?>" id="<?php echo md5($term->slug); ?>" class="x-portfolio-filter"><?php echo $term->name; ?></a></li>
             <?php } } ?>
           </div>
           <div data-option-key="type">
             <li><a href="#" data-option-value="*" class="x-portfolio-filter selected"><?php _e( 'All', '__x__' ); ?></a></li>
               <li><a href="#" data-option-value=".x-portfolio-b94ad99e5dc27b3365d5d2aa77d73ff0" class="x-portfolio-filter">Advocacy</a></li>
+              <li><a href="#" data-option-value=".x-portfolio-7412df2b1db8cd2a5d4aafdb6c2090d3" class="x-portfolio-filter">Research</a></li>
+              <li><a href="#" data-option-value=".x-portfolio-c185ddac8b5a8f5aa23c5b80bc12d214" class="x-portfolio-filter">Training</a></li>
          </div>
           </ul>
         </li>
