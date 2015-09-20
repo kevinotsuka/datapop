c9b018f4c8d9f53a1f9a29c9984149a434bd8b25
kevinotsuka <otsukasan@gmail.com>
2015-09-21 01:32:52 +0900

    updating to take out date and media type from portfolio

--- wp-content/themes/x/framework/views/renew/content-portfolio.php
+++ wp-content/themes/x/framework/views/renew/content-portfolio.php
@@ -16,11 +16,11 @@
       <?php x_portfolio_item_featured_content(); ?>
       <div class="entry-cover-nohover">
         <div class="entry-cover-content">
-          <span><?php echo get_post_meta( get_the_ID(), '_x_portfolio_media', true ); ?></span>
+          <!--- <span><?php //echo get_post_meta( get_the_ID(), '_x_portfolio_media', true ); ?></span> -->
           <h2 class="entry-title entry-title-portfolio">
             <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to: "%s"', '__x__' ), the_title_attribute( 'echo=0' ) ) ); ?>"><?php x_the_alternate_title(); ?></a>
           </h2>
-          <span><?php echo get_the_date( 'm.d.y' ); ?></span>
+          <!---<span><?php //echo get_the_date( 'm.d.y' ); ?></span> -->
         </div>
       </div>
     </div>
